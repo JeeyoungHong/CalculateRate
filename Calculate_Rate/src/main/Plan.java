3a1c0ad6c4800b0d02a97f8605530be8bee88618
Lee Yurim <fnladl72@naver.com>
2016-03-31 13:12:41 +0900

    Team19

---
+++ Calculate_Rate/src/main/Plan.java
@@ -0,0 +1,35 @@
+public class Plan {
+	private double excessminute_rate;
+	private double includedMinutes;
+	private double additional_rate;
+	private double planrate;
+	Gold gold;
+	Silver silver;
+	public Plan(User user){
+		if(user.getType().equals("gold")){
+			gold = new Gold();
+			this.excessminute_rate = gold.getExcessminute_rate();
+			this.includedMinutes = gold.getIncludedMinutes();
+			this.additional_rate = gold.getAdditional_rate();
+			this.planrate = gold.getPlanrate();
+		}else if(user.getType().equals("silver")){
+			silver = new Silver();
+			this.excessminute_rate = silver.getExcessminute_rate();
+			this.includedMinutes = silver.getIncludedMinutes();
+			this.additional_rate = silver.getAdditional_rate();
+			this.planrate = silver.getPlanrate();
+		}
+	}
+	public double getExcessminute_rate() {
+		return excessminute_rate; //추가시간에대한 요금
+	}
+	public double getIncludedMinutes() {
+		return includedMinutes; //기본시간
+	}
+	public double getAdditional_rate() {
+		return additional_rate;
+	}
+	public double getPlanrate() {
+		return planrate;
+	}
+}
---
+++ Calculate_Rate/src/main/TotalRate.java
@@ -0,0 +1,51 @@
+public class TotalRate {
+	private User user;
+	private Plan plan;
+	private double basic_monthly_rate;
+	private double excessminute;
+	private double familyrate;
+	static private double basic_family_rate;
+	private double additionalrate;
+	private double totalrate;
+
+	public TotalRate(User user) {
+		this.user = user;
+		plan = new Plan(user);
+		setCalculate();
+	}
+	private void setCalculate() {
+		totalrate = getExcessMinuteRate() + getAdditionalLineRate() + getFamilyrate() + getPlanRate();
+	}
+	public double getTotalrate() {
+		return totalrate;
+	}
+	public double getExcessMinuteRate() {
+		if(user.getMinutesofuse() > plan.getIncludedMinutes()) {
+			double temp = user.getMinutesofuse()-plan.getIncludedMinutes();
+			this.excessminute= temp*plan.getExcessminute_rate();
+		}
+		else
+			this.excessminute= 0;
+		return excessminute;
+	}
+	public double getAdditionalLineRate() {
+		int temp = user.getNumberofline()-1;
+		if(temp<3){
+			additionalrate = plan.getAdditional_rate()*temp;
+		}else{
+			additionalrate = plan.getAdditional_rate()*2;
+		}
+		return additionalrate;
+	}
+	public double getFamilyrate() {
+		if (user.getNumberofline()>3){ 
+			familyrate = (user.getNumberofline() - 3)*5;
+		}
+		else familyrate = 0;
+
+		return familyrate;
+	}
+	public double getPlanRate() {
+		return plan.getPlanrate();
+	}
+}
